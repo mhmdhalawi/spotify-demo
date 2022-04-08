fef95a05a5b3820dcc0b8f012311d24d0610973d
mhmdhalawi <mohammadhalawi93@gmail.com>
2022-04-08 18:35:51 +0300

    add home page plus search comp

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,50 @@
+import { TextInput, Grid } from '@mantine/core';
+import { useEffect, useState } from 'react';
+
+import { BiSearchAlt2 } from 'react-icons/bi';
+import { GrFormClose } from 'react-icons/gr';
+
+import { useDebouncedValue } from '@mantine/hooks';
+
+const Search = () => {
+  const [search, setSearch] = useState('');
+
+  const [debounced] = useDebouncedValue(search, 250);
+
+  const clearSearch = () => {
+    setSearch('');
+  };
+
+  useEffect(() => {
+    console.log({ debounced });
+  }, [debounced]);
+
+  return (
+    <Grid
+      justify={'center'}
+      m={0}
+      style={{
+        transform: `${search ? 'translateY(0px)' : 'translateY(300px) '}`,
+        transition: 'all 0.25s ease-in',
+      }}
+    >
+      <Grid.Col md={4} style={{ margin: 0 }}>
+        <TextInput
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          size='lg'
+          placeholder='Search for an artist…'
+          rightSection={
+            search ? (
+              <GrFormClose style={{ cursor: 'pointer' }} size={18} onClick={clearSearch} />
+            ) : (
+              <BiSearchAlt2 size={18} />
+            )
+          }
+        />
+      </Grid.Col>
+    </Grid>
+  );
+};
+
+export default Search;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from '../components/Search';
+
+const Homepage = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+
+export default Homepage;
